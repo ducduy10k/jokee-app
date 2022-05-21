a7e9eedb619d98a0350fbb80e68c632269f64f78
ducduy10k <104606418+rikkei-duynd@users.noreply.github.com>
2022-05-21 18:45:03 +0700

    init

---
+++ src/components/common/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Header = (props) => {
+  return (
+    <div className="header">
+      <div className="logo-header">
+        <img src="https://gudlogo.com/wp-content/uploads/2019/04/logo-hoa-huong-duong-11-1030x772.png" />
+      </div>
+      <div className="info">
+        <div className="profile">
+          <div style={{fontSize:'.8rem', color:'#b0b0b0'}}>Handicrafted by</div>
+          <div className="profile-name">Jim HLS</div>
+        </div>
+        <div className="avatar">
+          <img src="https://hoayeuthuong.com/cms-images/flower-meaning/434019_y-nghia-hoa-huong-duong.jpg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
---
+++ src/components/layout/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "../common/header";
+import Footer from "../common/footer";
+const Main = (props) => {
+  return (
+    <>
+      <Header />
+      <div>main</div>
+      <Footer />
+    </>
+  );
+};
+
+Main.propTypes = {};
+
+export default Main;
